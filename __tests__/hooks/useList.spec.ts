69922be15b14d53d5f170847835b7c7b7295c55c
Omar Urquidez <om4r4rm4@gmail.com>
2025-05-23 23:18:31 -0700

    new test case

--- __tests__/hooks/useList.spec.ts
+++ __tests__/hooks/useList.spec.ts
@@ -1,9 +1,12 @@
 import useList from "@/hooks/useList";
+import { mockPBClient } from "@/utils/testing";
 import { renderHook } from "@testing-library/react-hooks/native";
 
 describe("useList", () => {
   it("should fetch data successfully", async () => {
-    const { result, waitForNextUpdate } = renderHook(() => useList("users"));
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useList("users", {}, mockPBClient)
+    );
 
     let [users, { status }] = result.current;
 
@@ -18,5 +21,23 @@ describe("useList", () => {
 
     console.log("users", users);
     console.log("test");
+  });
+
+  it("should throw be error for invalid collection", async () => {
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useList("invalid-collection" as any, {}, mockPBClient)
+    );
+
+    let [_, { status, error }] = result.current;
+
+    expect(status).toBe("loading");
+
+    await waitForNextUpdate();
+
+    [_, { status, error }] = result.current;
+
+    expect(status).toBe("error");
+
+    console.log("Error", error);
   });
 });
--- src/utils/testing.ts
+++ src/utils/testing.ts
@@ -10,14 +10,33 @@ export interface PBClient {
   };
 }
 
-const mockUser: User = {
-  id: "user123",
-  name: "Juan Pérez",
-  email: "test@gmail.com",
-  avatar: "https://example.com/avatar.jpg",
-  emailVisibility: true,
-  role: "client",
+export const mockPBClient: PBClient = {
+  collection: (collection: string) => ({
+    getFullList: async <T>(options?: any): Promise<T[]> => {
+      if (collection === "users") return mockUsers as unknown as T[];
+      else throw new Error("Invalid collection");
+    },
+  }),
 };
+
+const mockUsers: User[] = [
+  {
+    id: "user123",
+    name: "Juan Pérez",
+    email: "test@gmail.com",
+    avatar: "https://example.com/avatar.jpg",
+    emailVisibility: true,
+    role: "client",
+  },
+  {
+    id: "user456",
+    name: "María López",
+    email: "a@atac.dev",
+    avatar: "https://example.com/avatar2.jpg",
+    emailVisibility: false,
+    role: "client",
+  },
+];
 
 export const mockNotifications: Notification[] = [
   {
@@ -29,7 +48,7 @@ export const mockNotifications: Notification[] = [
     created: "2025-05-18T10:30:00Z",
     updated: "2025-05-18T10:30:00Z",
     expand: {
-      user: mockUser,
+      user: mockUsers[0],
     },
   },
   {
@@ -41,7 +60,7 @@ export const mockNotifications: Notification[] = [
     created: "2025-05-17T15:45:00Z",
     updated: "2025-05-17T16:00:00Z",
     expand: {
-      user: mockUser,
+      user: mockUsers[0],
     },
   },
   {
@@ -53,7 +72,7 @@ export const mockNotifications: Notification[] = [
     created: "2025-05-16T09:20:00Z",
     updated: "2025-05-16T09:20:00Z",
     expand: {
-      user: mockUser,
+      user: mockUsers[0],
     },
   },
   {
@@ -65,7 +84,7 @@ export const mockNotifications: Notification[] = [
     created: "2025-05-15T08:00:00Z",
     updated: "2025-05-15T08:00:00Z",
     expand: {
-      user: mockUser,
+      user: mockUsers[0],
     },
   },
 ];
